d2759e9ecd03ac90277da39688c17aa8d6b80fc2
王保清 <viprosite@outlook.com>
2023-07-19 14:36:50 +0800

    'init'

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="header__container">
+        header 123 {{ a }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            a: 1
+        }
+    },
+    created() {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.header__container {
+    color: gray;
+}
+</style>
